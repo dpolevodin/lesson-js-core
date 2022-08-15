6c225dc4eafe462b16bf5e11fb72b0bae4124913
Дмитрий Полеводин <digpolevodin@sberbank.ru>
2022-08-15 23:44:04 +0500

    added parallax

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-const button = document.querySelector('.button')
+const container1 = document.querySelector('.container-primary')
+const container2 = document.querySelector('.container-secondary')
+const container3 = document.querySelector('.container-transparent')
 
-button.addEventListener('click', () => {
-    document
-        .querySelector('.modal')
-        .setAttribute('class', 'modal')
-})
-
-const closeButton = document.querySelector('.modal__button')
-
-closeButton.addEventListener('click', () => {
-    document
-        .querySelector('.modal')
-        .setAttribute('class', 'modal modal_hidden')
+document.addEventListener('scroll', () => {
+    const valueScrollY = window.scrollY
+    container1.style.top = valueScrollY * 0.5 + 'px'
+    container2.style.top = valueScrollY * 1.5 + 'px'
+    container2.style.left = valueScrollY * 1.5 + 'px'
+    container3.style.top = valueScrollY * 2.5 + 'px'
 })
