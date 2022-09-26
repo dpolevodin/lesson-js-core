884ae5e3e35b26c2d355e8fc10f9943fa0236224
Дмитрий Полеводин <digpolevodin@sberbank.ru>
2022-09-26 13:45:50 +0600

    added capture

--- script_dom.js
+++ script_dom.js
@@ -4,26 +4,23 @@ const cardsButtons = document.querySelectorAll('.card__button')
 cards.forEach(card => card.addEventListener('click', () => {
     if (card.classList.contains('card_active')) {
         card.setAttribute('class', 'card')
-        console.log('карточка удалена из выбора')
+        console.log('карточка удалена из выбора покупателя')
     }
     else {
         card.setAttribute('class', 'card card_active')
         console.log('выбрана карточка')
     }
-    })
+    }, false)
 )
 
 cardsButtons.forEach(card => card.addEventListener('click', (event) => {
 if (card.classList.contains('card__button_danger')) {
-    console.log(event)
-    // event.stopPropagation()
+    event.stopPropagation()
     console.log('удалить товар')
 }
 else {
-    // event.stopPropagation();
+    event.stopPropagation();
     console.log('купить товар')
-}
-})
-)
+}}))
 
 
